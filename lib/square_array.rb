b9012ae60216c93b7a73e0057f5cb677faec299f
Cane Rosso <astor@usa.com>
2019-11-07 19:14:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,9 @@
 #building a method, square_array, that squares each element in an array of numbers and returns a new array of these squared numbers.
 
 def square_array(array)
-  # your code here
-  # array  length 
+  counter
+  while array[counter]
   
-  i = 0
-  j = 0
-         while(j<i){
-              System.out.println(arr[i]);
-              i++;
+  newarray[counter] = 
   
 end
