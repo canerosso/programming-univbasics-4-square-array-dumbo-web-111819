3b927d765a93b6a92ed7d540db02ff246c21d2d3
Cane Rosso <astor@usa.com>
2019-11-07 19:21:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,11 @@
 #building a method, square_array, that squares each element in an array of numbers and returns a new array of these squared numbers.
 
 def square_array(array)
-  counter
+  counter = 0
   while array[counter]
   
   newarray[counter] = array[counter]*array[counter]
-  cou
+  counter += 1
+end
+
 end
