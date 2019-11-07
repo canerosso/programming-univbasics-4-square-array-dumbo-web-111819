0f74eae17e824b51617ef72e3c6a0b8d31103c28
Cane Rosso <astor@usa.com>
2019-11-07 19:23:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,8 +4,8 @@ def square_array(array)
   counter = 0
   while array[counter]
   
-  newarray[counter] = array[counter]*array[counter]
+  array[counter] = array[counter]*array[counter]
   counter += 1
 end
-
+array
 end
